37faa2c96a971903fb4115565044946077b6c4e8
joaomf <joaomf_24@hotmail.com>
2015-12-01 01:33:04 +0000

    Janela GS em produção: elementos já colocados

--- pa.iscde.gs/src/pa/iscde/gs/GSTool.java
+++ pa.iscde.gs/src/pa/iscde/gs/GSTool.java
@@ -1,7 +1,13 @@
 package pa.iscde.gs;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Point;
@@ -13,7 +19,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.BorderFactory;
-import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.*;
 import javax.swing.JPanel;
 
 import org.osgi.framework.Bundle;
@@ -61,6 +68,12 @@ public class GSTool implements PidescoTool {
 		final JFrame window = new JFrame("Getters and Setters");
 		window.setLayout(new GridLayout(0,1));
 		JPanel panel = createPanel();
+		JPanel panel_checkbox = createCheckBoxPanel();
+		JPanel panel_buttons = setPanelButtons();
+		JPanel panel_tostring = createPaneltoString();
+		panel.add(panel_checkbox);
+		panel.add(panel_tostring);
+		panel.add(panel_buttons);
 		window.getContentPane().add(panel);
 		window.setSize(400, 600);
 		window.setLocation(300 , 0 );
@@ -78,21 +91,7 @@ public class GSTool implements PidescoTool {
 
 			@Override
 			public void componentMoved(ComponentEvent e) {
-				final Point p = e.getComponent().getLocation();
-				// only triggered by a manual window move
-				/*if(p.x != model.getX() || p.y != model.getY()) {
-					WindowCommand cmd = new WindowCommand() {
-						
-						public String name() {
-							return "position update";
-						}
-						
-						public void run(WindowModel model) {
-							model.setPosition(p.x, p.y);
-						}
-					};
-					model.runCommand(cmd);
-				}*/
+				
 			}
 
 			@Override
@@ -111,18 +110,74 @@ public class GSTool implements PidescoTool {
 
 	private JPanel createPanel() {
 		JPanel panel = new JPanel();
-		//Label label = new Label("Getters and Setters");
-		//panel.add(label);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		Label label = new Label("Select Getters and Setters to create");
+		panel.add(label);
+		
+		
+		return panel;
+	}
+	
+	private JPanel createCheckBoxPanel() {
+		
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setPreferredSize(new Dimension(200, 100));
+		panel.setBackground(Color.WHITE);
+		
+		JCheckBox cb1 = new JCheckBox( "Test" );
+        cb1.setBackground(Color.WHITE);
+        cb1.addActionListener(new ActionListener() {
 
-		/*Button button = new Button("undo");
-		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Entrou");
+			}
+		});
+
+	    panel.add(cb1, BorderLayout.WEST);
+	    panel.add(cb1, BorderLayout.NORTH);
+		return panel;
+	}
+	private JPanel createPaneltoString(){
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		String[] choices = { "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+
+	    final JComboBox<String> cb = new JComboBox<String>(choices);
+
+	    cb.setVisible(true);
+	    Label label = new Label("Insertion point:");
+		panel.add(label);
+	    panel.add(cb);
+	    
+	    return panel;
+		
+	}
+	private JPanel setPanelButtons(){
+		JPanel panel = new JPanel();
+		JButton ok = new JButton("OK");
+		
+		ok.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//model.undo();
 			}
-		});*/
+		});
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//model.undo();
+			}
+		});
+		
+		panel.add(ok);
+		panel.add(cancel);
 		return panel;
+		
+		
 	}
 	
 }
